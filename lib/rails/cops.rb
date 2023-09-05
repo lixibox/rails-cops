9ab1123ac4496b5f05007d9868cae17a45de61a2
Alex <63999152+tungmq@users.noreply.github.com>
2023-09-05 11:34:25 +0700

    init gem

---
+++ lib/rails/cops.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "cops/version"
+
+module Rails
+  module Cops
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rails/cops/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Rails
+  module Cops
+    VERSION = "0.1.0"
+  end
+end
